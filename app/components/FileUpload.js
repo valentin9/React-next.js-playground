2b7b6ca1767472e4b103a3972792739432479da9
Valentin Derksen <valentin.derksen@statista.com>
2020-07-12 09:44:04 +0200

    upload errors improved

--- app/components/FileUpload.js
+++ app/components/FileUpload.js
@@ -4,33 +4,46 @@ import { FileRepository } from '../repositories/FileRepository';
 import FileInput from './FormFileInput';
 
 export default () => {
-    const MAX_SIZE_ERROR_MESSAGE = 'The file is too big, try another one!';
+    const MAX_SIZE_ERROR_MESSAGE = 'The file is over 10MB, try another one!';
+    const FILE_EXISTS_ERROR_MESSAGE = 'The file with same name already exists!';
 
     const { setFiles } = useContext(FilesContext);
     const [showMaxSizeError, setShowMaxSizeError] = useState(false);
-    const SIZE_LIMIT_10MB = 100 * 1024;
+    const [showExistsMessage, setShowExistsMessage] = useState(false);
+    const SIZE_LIMIT_10MB = 10 * 1024 * 1024;
+    const HTTP_STATUS_CONFLICT = 409;
+
+    const resetMessages = () => {
+        setShowMaxSizeError(false);
+        setShowExistsMessage(false);
+    };
 
     const handleChangeFile = event => {
         event.preventDefault();
+        resetMessages();
         const file = event.target.files[0];
 
         if (!file) {
-            setShowMaxSizeError(false);
-
             return;
         }
 
         if (file.size > SIZE_LIMIT_10MB) {
             event.target.value = '';
             setShowMaxSizeError(true);
-        } else {
-            setShowMaxSizeError(false);
-            FileRepository.uploadFile(file)
-                .then(response => {
-                    setFiles(response.data);
-                })
-                .catch(response => console.warn(response));
+
+            return;
         }
+
+        FileRepository.uploadFile(file)
+            .then(response => {
+                event.target.value = '';
+                setFiles(response.data);
+            })
+            .catch(error => {
+                if (error.response.status === HTTP_STATUS_CONFLICT) {
+                    setShowExistsMessage(true);
+                }
+            });
     };
 
     return (
@@ -39,6 +52,7 @@ export default () => {
                 Upload
             </FileInput>
             {showMaxSizeError && MAX_SIZE_ERROR_MESSAGE}
+            {showExistsMessage && FILE_EXISTS_ERROR_MESSAGE}
         </>
     );
 };
